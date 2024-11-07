13b04fd3f2f7a7e443f7c260427cdce3aa2759e6
dustlr <joe6069@naver.com>
2024-11-07 10:47:36 +0900

    인터페이스 푸드

---
+++ untitled2/src/kr/co/Main.java
@@ -0,0 +1,39 @@
+package kr.co;
+interface A{
+    public void funcA();
+}
+interface B{
+    public void funcB();
+}
+interface C extends A,B{
+    public void funcC();
+}
+class D implements C{
+    public void funcA(){
+        System.out.println("A");
+    }
+    public void funcB(){
+        System.out.println("A");
+    }
+    public void funcC(){
+        System.out.println("A");
+    }
+}
+public class Main {
+    public static void main(String[] args){
+        D d1= new D(); //up
+        A a1=d1;
+        a1.funcA();
+
+        B b1=d1;        //up
+        b1.funcB();
+
+        C c1=d1;        //up
+        c1.funcA();
+        c1.funcB();
+        c1.funcC();
+        //인터페이스에서 객채 생성불가하기 때문에
+        //상속받은 클래스D에서 객체생성
+
+    }
+}
